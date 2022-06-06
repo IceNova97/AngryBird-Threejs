4bff0d951e37ca84e49a91677994616a09dcc26c
胡彦斌 <huyanbin@git.com>
2022-06-06 18:02:57 +0800

    基础模型搭建，接入cannon物理世界，小鸟的移动功能，鼠标键盘事件监听

---
+++ dist/bundle.311e5a266eca9281984f.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("hello");
+/******/ })()
+;
+//# sourceMappingURL=bundle.311e5a266eca9281984f.js.map
